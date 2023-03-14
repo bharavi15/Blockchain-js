e428dcb83e4350642e0cf98da5b0772e6ce1d3a4
Bharavi Lakhote <bharavi15@gmail.com>
2023-03-14 15:28:43 +0530

    initialised genesis block in redis

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import app from './app'
 import http from 'http'
-import { getRedisClient, getDbValue, setDbValue } from './dao'
+import { getRedisClient, getDbValue, setDbValue, flushDb } from './dao'
+import { Iblock } from './block'
 
 const sv = new http.Server(app)
 const port = process.env.PORT ?? 3000
@@ -16,4 +17,17 @@ async function initialize() {
   await getRedisClient()
   console.log(await setDbValue('asd','asfdgadsf'))
   console.log(await getDbValue('asd'))
+  await flushDb()
+  const GENESIS_BLOCK_HASH = process.env.GENESIS_BLOCK_HASH ?? '0000000000000000000000000000000000000000000000000000000000000000'
+  const CURRENT_BLOCK_HASH_CACHE_KEY = process.env.CURRENT_BLOCK_HASH_CACHE_KEY ?? 'block_current_hash'
+  const BLOCK_HASH_CACHE_PREFIX = process.env.BLOCK_HASH_CACHE_PREFIX ?? 'block:currentHash:'
+
+  const genesisBlock: Iblock = {
+  transactions: [],
+  prevHash: GENESIS_BLOCK_HASH,
+  currHash: GENESIS_BLOCK_HASH
+}
+
+await setDbValue(BLOCK_HASH_CACHE_PREFIX+GENESIS_BLOCK_HASH, JSON.stringify(genesisBlock))
+await setDbValue(CURRENT_BLOCK_HASH_CACHE_KEY,BLOCK_HASH_CACHE_PREFIX+GENESIS_BLOCK_HASH)
 }
