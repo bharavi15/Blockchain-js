591cbfd970f68e0ea4351c6ec74eb194cd9c2086
Bharavi Lakhote <bharavi15@gmail.com>
2023-03-14 15:26:30 +0530

    added flushdb method

--- dao.ts
+++ dao.ts
@@ -6,15 +6,28 @@ export async function getDbValue (key: string) {
   }
   const value = await client.get(key)
   console.log('key',key,'has value', value)
-  return value
+  return value??'{}'
 }
 
 export async function setDbValue (key: string, value: string) {
   if(!client.isReady){
     await getRedisClient()
   }
-
   return await client.set(key, value)
+}
+export async function delDbValue (key: string|null|undefined) {
+  if(key == null)
+    return
+  if(!client.isReady){
+    await getRedisClient()
+  }
+  return await client.del(key)
+}
+export async function flushDb () {
+  if(!client.isReady){
+    await getRedisClient()
+  }
+  return await client.flushDb()
 }
 export async function getRedisClient(){
   if(client.isReady){
